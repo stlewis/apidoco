263d761657c936202935d0c8ac4e244035eb996e
Steve Lewis <steve.l@lojistic.com>
2020-05-26 14:46:28 -0700

    Revert "Add a base_path config option and default to docs/api"
    This reverts commit 23387e8c779254c802033e2a8a28dfd4e4163262.

--- lib/apidoco.rb
+++ lib/apidoco.rb
@@ -8,8 +8,7 @@ require 'apidoco/file_parse_error'
 APIDOCO_TITLE = 'ApiDoco'
 
 module Apidoco
-  mattr_accessor :app_name, :auth_name, :auth_password, :base_path
+  mattr_accessor :app_name, :auth_name, :auth_password
 
   self.app_name = APIDOCO_TITLE
-  self.base_path = 'docs/api'
 end
