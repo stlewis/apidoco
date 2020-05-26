e761ed0b873e2ad0a9baf446b43093af839fdd43
Steve Lewis <steve.l@lojistic.com>
2020-05-26 14:49:04 -0700

    Add and use config option for version parser path

--- lib/apidoco.rb
+++ lib/apidoco.rb
@@ -8,7 +8,8 @@ require 'apidoco/file_parse_error'
 APIDOCO_TITLE = 'ApiDoco'
 
 module Apidoco
-  mattr_accessor :app_name, :auth_name, :auth_password
+  mattr_accessor :app_name, :auth_name, :auth_password, :base_path
 
   self.app_name = APIDOCO_TITLE
+  self.base_path = 'docs/api'
 end
